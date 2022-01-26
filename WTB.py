cb2163da794e1e5523e12443ab48964de70351c4
Vladimir <vlgu@mail.ru>
2022-01-26 18:16:10 +0300

    Переносы строк
    Немогу убрать переносы строк данные в файле  сохраняются через одну строку

--- WTB.py
+++ WTB.py
@@ -3,27 +3,33 @@ from tkinter import *
 
 def print_word():
     list_word.delete(0, END)
-    file = open("word_translate.txt", "r", encoding="utf-8")
+    file = open("sort_word_translate.txt", "r", encoding="utf-8")
     for i in file:
         list_word.insert(END, i)
         
-# add words in db    
 def add():
-    list = []
-    with open('word_translate.txt', 'r', encoding='utf-8') as file:
-        data = file.read().splitlines()
-        data.sort()
-        for i in data:
-            list.append(i)
     w = word.get()
     t = translate.get()
-    list.append(w+'-'+t)
-    list.sort()
-    with open('word_translate.txt', 'w', encoding='utf-8') as file:
-        for i in list:
-            file.write(i+'\n')
+    wt = w + '-' + t
+    infile = open('word_translate.txt', 'a', encoding='utf-8')
+    infile.writelines(wt+'\n')
+    infile.close()
+    sort()
     print_word()
-    clear_entry()
+
+def sort():
+    infile = open('word_translate.txt', 'r', encoding='utf-8')
+    outfile = open('sort_word_translate.txt', 'w', newline='', encoding='utf-8')
+    list = []
+    for i in infile:
+        list.append(i.strip()+'\n')
+    list.sort()
+    outfile.writelines(list)
+    infile.close()
+    outfile.close()
+    print(list)
+    
+
   
 def clear_entry():
     word.delete(0, END)
@@ -31,21 +37,17 @@ def clear_entry():
   
 
 def delite_word():
-    with open('word_translate.txt', 'w', encoding='utf-8') as file:
-        file.writelines('\n'.join(list_word.get(0, END)))
-           
-    # del_word = list_word.curselection()
-    # list_word.delete(del_word[0])
-    # data = list_word
-    # print(data)
-    # with open('word_translate.txt', 'w', encoding='utf-8') as file:
-    #     file.writelines(data)
+    del_word = list(list_word.curselection())
+    del_word.reverse()
+    for i in del_word:
+        list_word.delete(i)
+    infile = open('sort_word_translate.txt', 'w', encoding='utf-8')
+    infile.writelines('\n' .join(list_word.get(0, END)))
+    infile.close()
         
-  
-
 root = Tk()
 root.title('dictionary WTB')
-root.geometry('300x300+50+50')
+root.geometry('200x400+50+50')
 
 
 Label(root, text = 'word')     .grid(column=0, row=0, sticky=S, pady=(5,0),padx=(2,0))
@@ -61,7 +63,7 @@ delete = Button(root, text='delete', command= delite_word).grid(column=1, row=2,
 printW = Button(root, text='print', command= print_word).grid(column=2, row=2, columnspan=3, sticky=S, pady=(5,0),padx=(2,0))
 
 
-list_word = Listbox(root, height=30, width=30)
+list_word = Listbox(root, height=30, width=30, selectmode=EXTENDED)
 
 list_word.grid(column=0, row=4, columnspan=4, sticky=S, pady=(5,0),padx=(2,0))
 
--- print_file.py
+++ print_file.py
@@ -1,6 +1,16 @@
-with open('word_translate.txt', 'r', encoding='utf-8') as file:
-    data = file.readlines()
-del data[0]
+# with open('word_translate.txt', 'r', encoding='utf-8') as file:
+#     data = file.readlines()
+# del data[0]
 
-with open('word_translate.txt', 'w', encoding='utf-8') as file:
-    file.writelines(data)
+# with open('word_translate.txt', 'w', encoding='utf-8') as file:
+#     file.writelines(data)
+
+infile = open('word.txt', 'r')  # Файл с мусором
+outfile = open('sortedlines.txt', 'w', newline='')  # "Очищенный" файл
+lines = []
+for l in infile:
+    lines.append(l.strip()+'\n')
+lines.sort()
+outfile.writelines(lines)
+infile.close()
+outfile.close()
