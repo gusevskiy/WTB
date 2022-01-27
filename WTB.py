43e1bc377ca8a9e01df90e06962068500d29a8b3
Vladimir <vlgu@mail.ru>
2022-01-27 11:13:32 +0300

    Работает
    добавляет
    удаляет
    сохраняет
    и самое главное сортирует

--- WTB.py
+++ WTB.py
@@ -7,46 +7,56 @@ def print_word():
     for i in file:
         list_word.insert(END, i)
         
-def add():
+def one():
+    infile = open('word_translate.txt', 'r', encoding="utf-8") 
+    outfile = open('sort_word_translate.txt', 'w', encoding="utf-8")
+    lines = []
+    for l in infile:
+        lines.append(l.strip()+'\n')
+    lines.sort()
+    outfile.writelines(lines)
+    infile.close()
+    outfile.close()
+    
+def tu():
+    outfile = open('sort_word_translate.txt', 'r', encoding="utf-8")
+    infile = open('word_translate.txt', 'w', encoding='utf-8')
+    for i in outfile:
+        infile.write(i)
+    infile.close()
+    outfile.close()
+    
+def sort():
+    one()
+    tu()
+    
+def save_word():
     w = word.get()
     t = translate.get()
     wt = w + '-' + t
-    list_word.insert(END, wt)
-    clear_entry()
-    sort()
-         
-def sort():
-    infile = open('sort_word_translate.txt', 'r', encoding='utf-8')
-    outfile = open('word_translate.txt', 'w', newline='', encoding='utf-8')
-    list = []
-    for i in infile:
-        list.append(i.strip()+'\n')
-    list.sort()
-    outfile.writelines(list)
-    infile.close()
-    outfile.close()
-    print(list)
-    
-def save_word():
-    f = open('sort_word_translate.txt', 'w', encoding='utf-8')
-    f.writelines(list_word.get(0, END))
+    f = open('word_translate.txt', 'a', encoding='utf-8')
+    f.writelines('\n'+wt)
     f.close()
-  
-def clear_entry():
-    word.delete(0, END)
-    translate.delete(0, END)
-  
+    clear_entry()
+    print_word()
 
 def delite_word():
     del_word = list(list_word.curselection())
     del_word.reverse()
     for i in del_word:
         list_word.delete(i)
+    f = open('word_translate.txt', 'w', encoding='utf-8')
+    f.writelines((list_word.get(0,END)))
+    f.close()
+
+def clear_entry():
+    word.delete(0, END)
+    translate.delete(0, END)
     
         
 root = Tk()
 root.title('dictionary WTB')
-root.geometry('200x400+50+50')
+root.geometry('200x500+50+50')
 
 
 Label(root, text = 'word')     .grid(column=0, row=0, sticky=S, pady=(5,0),padx=(2,0))
@@ -57,7 +67,7 @@ word.grid(column=1, row=0, columnspan = 3, sticky=S, pady=(5,0),padx=(2,0))
 translate = Entry(root, width=20)
 translate.grid(column=1, row=1, columnspan = 3, sticky=S, pady=(5,0),padx=(2,0))
 
-create = Button(root, text='add', command= add).grid(column=0, row=2, sticky=S, pady=(5,0),padx=(2,0))
+create = Button(root, text='sort', command= sort).grid(column=0, row=2, sticky=S, pady=(5,0),padx=(2,0))
 delete = Button(root, text='delete', command= delite_word).grid(column=1, row=2, sticky=S, pady=(5,0),padx=(2,0))
 printW = Button(root, text='print', command= print_word).grid(column=2, row=2, sticky=S, pady=(5,0),padx=(2,0))
 printW = Button(root, text='save', command= save_word).grid(column=3, row=2, sticky=S, pady=(5,0),padx=(2,0))
--- print_file.py
+++ print_file.py
@@ -4,13 +4,24 @@
 
 # with open('word_translate.txt', 'w', encoding='utf-8') as file:
 #     file.writelines(data)
-
-infile = open('word.txt', 'r')  # Файл с мусором
-outfile = open('sortedlines.txt', 'w', newline='')  # "Очищенный" файл
-lines = []
-for l in infile:
-    lines.append(l.strip()+'\n')
-lines.sort()
-outfile.writelines(lines)
-infile.close()
-outfile.close()
+def one():
+    infile = open('word.txt', 'r')  # Файл с мусором
+    outfile = open('sortedlines.txt', 'w')  # "Очищенный" файл
+    lines = []
+    for l in infile:
+        lines.append(l.strip()+'\n')
+    lines.sort()
+    outfile.writelines(lines)
+    infile.close()
+    outfile.close()
+    
+def tu():
+    outfile = open('sortedlines.txt', 'r')
+    infile = open('word.txt', 'w')
+    for i in outfile:
+        infile.write(i)
+    infile.close()
+    outfile.close()
+    
+one()
+tu()
