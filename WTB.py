ae13a0975d5c7c0030cf9069f938b3557e3c45a9
Vladimir <vlgu@mail.ru>
2022-02-02 13:05:13 +0300

    FINAL
    убрал промежуточный файл
    завязал все действия на одной кнопке.
    Добавил выполнение Enter/
    поработать над внешним видом остается

--- WTB.py
+++ WTB.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 def print_word():
     if letter.get() == '':
-        sort()
+        #sort()
         list_word.delete(0, END)
         with open("word_translate.txt", "r", encoding="utf-8") as file:
             for i in file:
@@ -13,44 +13,28 @@ def print_word():
         let = letter.get()
         with open('word_translate.txt', 'r', encoding='utf-8') as file:
             for i in file:
-                if i.startswith(let):
+                if i.startswith(let): #если строка в файле начинается на let вывод ее в list_word т.е. на экран
                     list_word.insert(END, i)
         letter.delete(0, END)
         
-        
-def one():
-    infile = open('word_translate.txt', 'r', encoding="utf-8") 
-    outfile = open('sort_word_translate.txt', 'w', encoding="utf-8")
-    lines = []
-    for l in infile:
-        lines.append(l.strip()+'\n')
-    lines.sort()
-    outfile.writelines(lines)
-    infile.close()
-    outfile.close()
-    
-def tu():
-    outfile = open('sort_word_translate.txt', 'r', encoding="utf-8")
-    infile = open('word_translate.txt', 'w', encoding='utf-8')
-    for i in outfile:
-        infile.write(i)
-    infile.close()
-    outfile.close()
-    
-def sort():
-    one()
-    tu()
-    
 def save_word():
     if word.get() == '' and translate.get() == '':
+        print_word()
         return None
+    
     else:
+        words = []
+        with open('word_translate.txt', 'r', encoding='utf-8') as f:
+            for i in f:
+                words.append(i)
         w = word.get()
         t = translate.get()
         wt = w + ' - ' + t
-        f = open('word_translate.txt', 'a', encoding='utf-8')
-        f.writelines(wt)
-        f.close()
+        words.append(wt+'\n')
+        words.sort()
+        with open('word_translate.txt', 'w', encoding='utf-8') as f:
+            for i in words:
+                f.write(i)
         clear_entry()
         print_word()
 
@@ -65,6 +49,9 @@ def delite_word():
     
 def del_keyboard(self):# функция специально для кнопки Delete
     delite_word()
+    
+def enter_perform(self):# функция специально для кнопки Enter
+    save_word()    
 
 def clear_entry():
     word.delete(0, END)
@@ -73,30 +60,30 @@ def clear_entry():
         
 root = Tk()
 root.title('dictionary WTB')
-root.geometry('280x750+50+50')
+root.geometry('350x900+50+50')
 
 Label(root, text = 'word')     .grid(column=0, row=0, sticky=S, pady=(5,0),padx=(2,0))
 Label(root, text = 'translate').grid(column=0, row=1, sticky=S, pady=(5,0),padx=(2,0))
+Label(root, text = 'search for a word').grid(column=0, row=2, sticky=S, pady=(5,0),padx=(2,0))
 
 scroll_bar = Scrollbar(root)
 scroll_bar.grid(column=3,row=3, sticky='ns')
 
-word = Entry(root, width=30)
-word.grid(column=1, row=0, sticky=S, pady=(5,0),padx=(2,0))
-translate = Entry(root, width=30)
-translate.grid(column=1, row=1, sticky=S, pady=(5,0),padx=(2,0))
-letter = Entry(root, width=2)
-letter.grid(column=1, row=2, pady=(5,0),padx=(2,0))
+word = Entry(root, width=36)
+word.grid(column=1, row=0, sticky=W, pady=(5,0),padx=(2,0))
+translate = Entry(root, width=36)
+translate.grid(column=1, row=1, sticky=W, pady=(5,0),padx=(2,0))
+letter = Entry(root, width=10)
+letter.grid(column=1, row=2, sticky=W, pady=(5,0),padx=(2,0))
 
-print_bottum = Button(root, text='print', command= print_word, height=1, width=8)
-print_bottum.grid(column=1, row=2, sticky=E, pady=(5,0),padx=(2,0))
-save_bottom = Button(root, text='save', command= save_word, height=1, width=8)
-save_bottom.grid(column=1, row=2, sticky=W, pady=(5,0),padx=(2,0))
+save_bottom = Button(root, text='perform', command= save_word, height=1, width=20)
+save_bottom.grid(column=1, row=2, sticky=E, pady=(5,0),padx=(2,0))
 
 root.bind('<Delete>', del_keyboard)
+root.bind('<Return>', enter_perform)
 
-list_word = Listbox(root, height=40, width=42, selectmode=SINGLE)
-list_word.grid(column=0, row=3, columnspan=2, pady=(5,0),padx=(2,0))
+list_word = Listbox(root, height=42, width=45, selectmode=SINGLE, selectborderwidth=2, font=('Consolas', '9'))
+list_word.grid(column=0, row=3, columnspan=2, pady=1,padx=1, ipadx=1, ipady=1)
 list_word.config(yscrollcommand = scroll_bar.set)
 scroll_bar.config(command=list_word.yview)
 
